649720a1965185307b0428826849a8a09adbca74
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-11-12 19:35:22 -0500

    Add todo-comments plugin & display path to file at top of buffer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Show relative path to file at the top of buffer
+vim.opt.winbar = "%=%m %f"
