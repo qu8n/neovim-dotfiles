22b4597a22e57a19e9d620c51d042fc37bc36901
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2024-11-25 17:21:13 -0500

    Show sidebar on the right

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,9 @@
 return {
   "neo-tree.nvim",
   opts = {
+    -- Show sidebar on the right
+    window = { position = "right" },
+
     filesystem = {
       filtered_items = {
         visible = true,
