927333403da6dcd712f1eca5b302bd2e789298c0
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2025-02-04 13:38:04 -0500

    Disable lsp_lines by default

--- lua/plugins/lsp-lines.lua
+++ lua/plugins/lsp-lines.lua
@@ -1,4 +1,8 @@
 -- Show diagnostics under the line instead of next to for improved readability
 return {
-  "https://git.sr.ht/~whynothugo/lsp_lines.nvim"
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  config = function()
+    require("lsp_lines").setup()
+    vim.diagnostic.config({ virtual_lines = false }) -- disabled by default
+  end,
 }
